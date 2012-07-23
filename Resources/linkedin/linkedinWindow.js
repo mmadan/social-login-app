7951db6d622150240e3b79b7c8b574f3cc9bc946
Your Name <arpit.sharma@oodlestechnologies.com>
2012-07-23 19:26:49 +0530

    linkedin implementation

--- Resources/linkedin/linkedinWindow.js
+++ Resources/linkedin/linkedinWindow.js
@@ -9,10 +9,27 @@ exports.createLinkedinWindow = function() {
 		width : '50%',
 		top : '50dp'
 	});
-	linkedInButton.addEventListener('click', function() {
-
-	});
 	win.add(linkedInButton);
+	var login = false;
+	linkedInButton.addEventListener('click', function(){
+		if(!login)
+		{
+			require('/ui/animateWindow').createAnimateWindow(linkedin_share);
+		}else
+		{
+			Ti.App.Properties.setString("linkedin_token", "");
+			Ti.App.Properties.setString("linkedin_token_secret", "");
+			Ti.App.Properties.setString("linkedin_pin","");
+			linkedInButton.title = "login";
+			login = false;
+		}
+	});
+	var linkedin_share = function()
+	{
+		linkedInButton.title = "LinkedIn Logout";
+		login = true;
+	}
+
 
 	return win;
 }
